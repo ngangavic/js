f670ee5c020e97b77719e49ce3f4ce7e8e42ebef
ngangavic <ngangavictor10@gmail.com>
2020-01-07 18:14:38 +0300

    re-using component

--- auto-complete-text/src/App.js
+++ auto-complete-text/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import AutoCompleteText from './AutoCompleteText';
+import countries from "./countries";
 
 function App() {
     return (
         <div className="App">
             <div className="App-Component">
                 <div className="App-Component">
-                    <AutoCompleteText/>
+                    <AutoCompleteText items={countries}/>
+                <br/>
+                <br/>
+                    <AutoCompleteText items={['countries','Animals','People']}/>
                 </div>
             </div>
         </div>
