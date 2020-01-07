6512b1f80bfaa2314b893e75dbbfaecb20626efe
ngangavic <ngangavictor10@gmail.com>
2020-01-07 18:07:20 +0300

    working with css in react

--- auto-complete-text/src/App.js
+++ auto-complete-text/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import AutoCompleteText from './AutoCompleteText';
 
 function App() {
-  return (
-    <div className="App">
-      <AutoCompleteText/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="App-Component">
+                <div className="App-Component">
+                    <AutoCompleteText/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
