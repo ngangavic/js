117929dc537031ea75178748a7a85a506a332af1
ngangavic <ngangavictor10@gmail.com>
2020-02-06 16:39:22 +0300

    connecting to chat-kit API

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import ChatKit from '@pusher/chatkit-client-react';
+import RoomList from '../components/RoomList';
+import MessageList from '../components/MessageList';
+import SendMessageForm from '../components/SendMessageForm';
+import NewRoomForm from '../components/NewRoomForm';
 import './App.css';
+import { tokenUrl, instanceLocator } from './config';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+class App extends React.Component {
+
+  componentDidMount() {
+    const chatManager = new ChatKit.ChatManager({
+      instanceLocator: instanceLocator,
+      userId: 'vic',
+      tokenProvider: new ChatKit.TokenProvider({
+        url: tokenUrl
+      })
+    })
+    chatManager.connect()
+      .then(currentUser => {
+        currentUser.subscribeToRoom({
+          roomId: 'c00dcc9a-51db-48f0-bad9-bfd17395f41b',
+          hooks: {
+            onNewMessage: message => {
+              console.log("message:", message.text);
+            }
+          }
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <RoomList />
+        <MessageList />
+        <SendMessageForm />
+        <NewRoomForm />
+      </div>
+    );
+  }
 }
 
 export default App;
