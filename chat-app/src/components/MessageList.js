ed1d3b4cc160b631bd90f1ac477e2067dbc9a954
ngangavic <ngangavictor10@gmail.com>
2020-02-06 16:48:27 +0300

    relocation

--- chat-app/src/components/MessageList.js
+++ chat-app/src/components/MessageList.js
@@ -1,25 +1,10 @@
 import React from 'react'
 
-const SAMPLE_DATA = [
-    {
-        senderId: 'Vic',
-        text: 'Hi. How are you?'
-    },
-    {
-        senderId: 'Mak',
-        text: 'Go code.'
-    },
-    {
-        senderId: 'Vic',
-        text: 'Always coding :-)'
-    }
-]
-
 class MessageList extends React.Component {
     render() {
         return (
             <div className="message-list">
-                {SAMPLE_DATA.map((message, index) => {
+                {this.props.messages.map((message, index) => {
                     return (
                         <div key={index} className="message">
                             <div className="message-username">{message.senderId}</div>
