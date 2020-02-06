f1b85fcd9041d88a3b7ed5fe8f9f34d68a0ca134
ngangavic <ngangavictor10@gmail.com>
2020-02-06 16:27:14 +0300

    config files urls

---
+++ chat-app/src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/02037853-1c44-4bee-85fd-b4f30db0f770/token";
+
+const instanceLocator = "v1:us1:02037853-1c44-4bee-85fd-b4f30db0f770";
+
+export { tokenUrl, instanceLocator }
