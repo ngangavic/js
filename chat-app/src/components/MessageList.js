006c6994ef5f1d33e9906d4b98a08b733a355503
ngangavic <ngangavictor10@gmail.com>
2020-02-11 16:00:06 +0300

    warning removal

--- chat-app/src/components/MessageList.js
+++ chat-app/src/components/MessageList.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 
 class MessageList extends React.Component {
 
-    componentWillUpdate() {
+    UNSAFE_componentWillUpdate() {
         const node = ReactDOM.findDOMNode(this)
         this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
     }
